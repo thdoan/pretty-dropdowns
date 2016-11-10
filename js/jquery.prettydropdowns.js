f1688272d148498a19d7c908ae0c5c7c1ecbae1d
Tom Doan <tomdoan@singpost.com>
2016-11-10 15:08:44 +0800

    Fix issues when select is initially hidden

--- js/jquery.prettydropdowns.js
+++ js/jquery.prettydropdowns.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery Pretty Dropdowns Plugin v1.0.2 by T. H. Doan (http://thdoan.github.io/pretty-dropdowns/)
+ * jQuery Pretty Dropdowns Plugin v1.0.5 by T. H. Doan (http://thdoan.github.io/pretty-dropdowns/)
  *
  * jQuery Pretty Dropdowns by T. H. Doan is licensed under the MIT License.
  * Read a copy of the license in the LICENSE file or at
@@ -14,13 +14,11 @@
       // NOTE: $this.css('margin') returns empty string in Firefox.
       // See https://github.com/jquery/jquery/issues/3383
       var nWidth = $this.outerWidth(),
-        nMarginL = $this.css('margin-left'),
-        nMarginR = $this.css('margin-right'),
         sHtml = '<ul' + ($this.attr('title')?' title="'+$this.attr('title')+'"':'') + ' style="margin:'
           + $this.css('margin-top') + ' '
-          + nMarginR + ' '
+          + $this.css('margin-right') + ' '
           + $this.css('margin-bottom') + ' '
-          + nMarginL + ';">',
+          + $this.css('margin-left') + ';">',
         resetDropdown = function(o) {
           var $dropdown = $(o.currentTarget||o);
           if ($dropdown.hasClass('reverse')) $dropdown.prepend($('li:last-child', $dropdown));
@@ -35,15 +33,18 @@
       sHtml += '</ul>';
       $this.css('visibility', 'hidden').wrap('<div class="prettydropdown loading"></div>').before(sHtml).data('loaded', true);
       var $dropdown = $('ul', $this.parent()),
-        nWidth = $dropdown.width();
+        nWidth = $dropdown.outerWidth(true),
+        nOuterWidth;
       // Calculate width if initially hidden
-      if (!nWidth) {
+      if (!$dropdown.width()) {
         var $clone = $dropdown.parent().clone().css({
             position: 'absolute',
             top: '-100%'
           });
         $('body').append($clone);
-        nWidth = $('ul', $clone).width();
+        nWidth = $('ul', $clone).outerWidth(true);
+        $('li', $clone).width(nWidth);
+        nOuterWidth = $('ul', $clone).outerWidth(true);
         $clone.remove();
       }
       // Set dropdown width and event handler
@@ -55,7 +56,7 @@
           $dropdown.prepend($li.addClass('chevron')).removeClass('reverse');
           // Sync <select> element
           $('option[value="' + $li.data('value') +'"]', $this).prop('selected', true);
-          $this.trigger('change');
+          //$this.trigger('change');
         }
         $dropdown.toggleClass('active');
         // Try to keep drop-down menu within viewport
@@ -74,7 +75,7 @@
         }
       });
       $dropdown.on('mouseleave', resetDropdown);
-      $dropdown.parent().width($('li', $dropdown).outerWidth()+parseFloat(nMarginL)+parseFloat(nMarginR)+2).removeClass('loading');
+      $dropdown.parent().width(nOuterWidth||$dropdown.outerWidth(true)).removeClass('loading');
     });
   };
 }(jQuery));
