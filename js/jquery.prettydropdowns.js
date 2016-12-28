a0a44d18dad1b00a9208816151af3ea040266784
Tom Doan <tomdoan@singpost.com>
2016-12-28 19:40:52 +0800

    Update to v2.0.0

--- js/jquery.prettydropdowns.js
+++ js/jquery.prettydropdowns.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery Pretty Dropdowns Plugin v1.0.6 by T. H. Doan (http://thdoan.github.io/pretty-dropdowns/)
+ * jQuery Pretty Dropdowns Plugin v2.0.0 by T. H. Doan (http://thdoan.github.io/pretty-dropdowns/)
  *
  * jQuery Pretty Dropdowns by T. H. Doan is licensed under the MIT License.
  * Read a copy of the license in the LICENSE file or at
@@ -7,31 +7,55 @@
  */
 
 (function($) {
-  $.fn.prettyDropdown = function() {
+  $.fn.prettyDropdown = function(oOptions) {
+    // Default options
+    oOptions = $.extend({
+      customClass: 'arrow',
+      height: 50,
+      hoverIntent: 200
+    }, oOptions);
+    var nTimer,
+      resetDropdown = function(o) {
+        var $dropdown = $(o.currentTarget||o);
+        $dropdown.data('hover', false);
+        clearTimeout(nTimer);
+        nTimer = setTimeout(function() {
+          if (!$dropdown.data('hover')) {
+            if ($dropdown.hasClass('reverse')) $dropdown.prepend($('li:last-child', $dropdown));
+            $dropdown.removeClass('active reverse').css('height', '');
+          }
+        }, (o.type==='mouseleave') ? oOptions.hoverIntent : 0);
+      };
+    // Validate options
+    if (isNaN(oOptions.height) || oOptions.height<8) oOptions.height = 8;
+    if (isNaN(oOptions.hoverIntent) || oOptions.hoverIntent<0) oOptions.hoverIntent = 200;
     return this.each(function() {
       var $this = $(this);
       if ($this.data('loaded')) return true; // Continue
+      $this.outerHeight(oOptions.height);
       // NOTE: $this.css('margin') returns empty string in Firefox.
       // See https://github.com/jquery/jquery/issues/3383
       var nWidth = $this.outerWidth(),
-        sHtml = '<ul' + ($this.attr('title')?' title="'+$this.attr('title')+'"':'') + ' style="margin:'
+        sHtml = '<ul' + ($this.attr('title')?' title="'+$this.attr('title')+'"':'') + ' style="max-height:' + (oOptions.height-2) + 'px;margin:'
           + $this.css('margin-top') + ' '
           + $this.css('margin-right') + ' '
           + $this.css('margin-bottom') + ' '
           + $this.css('margin-left') + ';">',
-        resetDropdown = function(o) {
-          var $dropdown = $(o.currentTarget||o);
-          if ($dropdown.hasClass('reverse')) $dropdown.prepend($('li:last-child', $dropdown));
-          $dropdown.removeClass('active reverse').css('height', '');
+        renderItem = function(el, sClass) {
+          return '<li data-value="' + el.value + '"'
+            + (el.title ? ' title="' + el.title + '"' : '')
+            + (sClass ? ' class="' + sClass + '"' : '')
+            + ((oOptions.height!==50) ? ' style="height:' + (oOptions.height-2) + 'px;line-height:' + (oOptions.height-2) + 'px"' : '')
+            + '>' + el.text + '</li>';
         };
       $('option:selected', $this).each(function() {
-        sHtml += '<li class="chevron" data-value="' + this.value + '">' + this.text + '</li>';
+        sHtml += renderItem(this, 'selected');
       });
       $('option:not(:selected)', $this).each(function() {
-        sHtml += '<li data-value="' + this.value + '">' + this.text + '</li>';
+        sHtml += renderItem(this);
       });
       sHtml += '</ul>';
-      $this.css('visibility', 'hidden').wrap('<div class="prettydropdown loading"></div>').before(sHtml).data('loaded', true);
+      $this.css('visibility', 'hidden').wrap('<div class="prettydropdown ' + oOptions.customClass + ' loading"></div>').before(sHtml).data('loaded', true);
       var $dropdown = $('ul', $this.parent()),
         nWidth = $dropdown.outerWidth(true),
         nOuterWidth;
@@ -51,9 +75,9 @@
       $('li', $dropdown).width(nWidth).click(function() {
         var $li = $(this);
         // Only update if different value selected
-        if ($dropdown.hasClass('active') && $li.data('value')!==$('li.chevron', $dropdown).data('value')) {
-          $('.chevron', $dropdown).removeClass('chevron');
-          $dropdown.prepend($li.addClass('chevron')).removeClass('reverse');
+        if ($dropdown.hasClass('active') && $li.data('value')!==$('li.selected', $dropdown).data('value')) {
+          $('.selected', $dropdown).removeClass('selected');
+          $dropdown.prepend($li.addClass('selected')).removeClass('reverse');
           // Sync <select> element
           $('option[value="' + $li.data('value') +'"]', $this).prop('selected', true);
           $this.trigger('change');
@@ -65,16 +89,30 @@
             nOffsetTop = $dropdown.offset().top,
             nScrollTop = document.body.scrollTop,
             nDropdownHeight = $dropdown.outerHeight(),
-            nDropdownBottom = nOffsetTop + nDropdownHeight - nScrollTop;
+            nDropdownBottom = nOffsetTop-nScrollTop+nDropdownHeight;
           if (nDropdownBottom>nWinHeight) {
-            if (nOffsetTop-nScrollTop>=nDropdownHeight-50) $dropdown.addClass('reverse').append($('li.chevron', $dropdown));
-            else $dropdown.height($dropdown.height()-(nDropdownBottom-nWinHeight));
+            // Expand to direction that has the most space
+            if (nOffsetTop-nScrollTop>nWinHeight-(nOffsetTop-nScrollTop+oOptions.height)) {
+              $dropdown.addClass('reverse').append($('li.selected', $dropdown));
+              if (nOffsetTop-nScrollTop+oOptions.height<nDropdownHeight) {
+                $dropdown.outerHeight(nOffsetTop-nScrollTop+oOptions.height);
+                $dropdown.scrollTop(nDropdownHeight);
+              }
+            } else {
+              $dropdown.height($dropdown.height()-(nDropdownBottom-nWinHeight));
+            }
           }
         } else {
           resetDropdown($dropdown[0]);
         }
       });
-      $dropdown.on('mouseleave', resetDropdown);
+      $dropdown.on({
+        mouseenter: function() {
+          $dropdown.data('hover', true);
+        },
+        mouseleave: resetDropdown
+      });
+      // Done with everything!
       $dropdown.parent().width(nOuterWidth||$dropdown.outerWidth(true)).removeClass('loading');
     });
   };
