756926fce554a3140eb119b286d287b3a932ad2e
Tom Doan <tomdoan@singpost.com>
2017-02-17 19:55:01 +0800

    Update to v3.3.0

--- index.md
+++ index.md
@@ -1,6 +1,13 @@
 # jQuery Pretty Dropdowns
 
-Pretty Dropdowns is a simple, lightweight jQuery plugin that converts `<select>` drop-down menus into "pretty" menus that you can style using CSS. Full keyboard navigation is supported (you can go directly to a menu item by typing its text). As an extra bonus, when you open the menu it does its best to keep the menu items within the viewport.
+Pretty Dropdowns is a simple, lightweight jQuery plugin that converts `<select>` drop-down menus into "pretty" menus that you can style using CSS.
+
+### Features:
+
+- Two arrow styles and sizes to choose from (or add your own style)
+- Full keyboard navigation support (you can even go directly to a menu item by typing its text)
+- Accessible (it plays nicely with screen readers)
+- Sensible (when you open the menu it does its best to keep the menu items within the viewport)
 
 **[See a demo &raquo;](https://thdoan.github.io/pretty-dropdowns/demo.html)**
 
@@ -50,17 +57,19 @@ Name          | Type   | Default | Description
 
 ## Keyboard Navigation
 
-Key     | Description
-------- | -----------
-`Enter` | Select highlighted item.
-`Esc`   | Close the drop-down menu.
-`Home`  | Jump to the first item in the drop-down menu.
-`End`   | Jump to the last item in the drop-down menu.
-`PgUp`  | Go to the previous page of items. If there is no scrollbar, then this is the same as `Home`.
-`PgDn`  | Go to the next page of items. If there is no scrollbar, then this is the same as `End`.
-`Up`    | Highlight the previous item in the drop-down menu.
-`Down`  | Highlight the next item in the drop-down menu.
-`A`-`Z`<br>`0`-`9`<br>`Space` | Jump to the first item matching the key(s) pressed. Every time you press a key it will cycle through the matching items. **Hint:** if you type fast enough, it will try to find a match for everything you typed instead of just the first character.
+Key     | Menu Closed | Menu Open
+------- | ----------- | ---------
+`Tab`   | Put focus on the next drop-down menu. |
+`Shift`+`Tab` | Put focus on the previous drop-down menu. |
+`Enter` | Open the drop-down menu. | Select the highlighted item.
+`Esc`   | | Close the drop-down menu.
+`Home`  | | Jump to the first item in the drop-down menu.
+`End`   | | Jump to the last item in the drop-down menu.
+`PgUp`  | | Go to the previous page of items. If there is no scrollbar, then this is the same as `Home`.
+`PgDn`  | | Go to the next page of items. If there is no scrollbar, then this is the same as `End`.
+`Up`    | | Highlight the previous item in the drop-down menu.
+`Down`  | | Highlight the next item in the drop-down menu.
+`A`-`Z`<br>`0`-`9`<br>`Space` | `Space` opens the drop-down menu. | Jump to the first item matching the key(s) pressed. Every time you press a key it will cycle through the matching items. **Hint:** if you type fast enough, it will try to find a match for everything you typed instead of just the first character.
 
 ## Installation
 
--- js/jquery.prettydropdowns.js
+++ js/jquery.prettydropdowns.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery Pretty Dropdowns Plugin v3.1.3 by T. H. Doan (http://thdoan.github.io/pretty-dropdowns/)
+ * jQuery Pretty Dropdowns Plugin v3.3.0 by T. H. Doan (http://thdoan.github.io/pretty-dropdowns/)
  *
  * jQuery Pretty Dropdowns by T. H. Doan is licensed under the MIT License.
  * Read a copy of the license in the LICENSE file or at
@@ -14,63 +14,77 @@
       height: 50,
       hoverIntent: 200
     }, oOptions);
-    var nHoverIndex,
-      nLastIndex,
-      nTimer,
+    var $current,
       aKeys = [
         '0','1','2','3','4','5','6','7','8','9',,,,,,,,
         'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
       ],
-      $current,
+      nHoverIndex,
+      nLastIndex,
+      nTimer,
       handleKeypress = function(e) {
-        var $dropdown = $('.prettydropdown > ul.active'),
+        var $dropdown = $('.prettydropdown > ul.active, .prettydropdown > ul:focus'),
+          $items = $dropdown.children(),
+          isOpen = $dropdown.hasClass('active'),
           nItemsHeight = $dropdown.height()/(oOptions.height-2),
           nItemsPerPage = nItemsHeight%1<0.5 ? Math.floor(nItemsHeight) : Math.ceil(nItemsHeight),
           sKey;
         if (!$dropdown.length) return;
-        e.preventDefault();
-        e.stopPropagation();
-        nHoverIndex = $dropdown.children('li.hover').index();
-        nLastIndex = $dropdown.children().length-1;
-        $current = $dropdown.children().eq(nHoverIndex);
+        if (e.which===9) { // Tab
+          resetDropdown($dropdown[0]);
+          return;
+        } else {
+          // Intercept non-Tab keys only
+          e.preventDefault();
+          e.stopPropagation();
+        }
+        nHoverIndex = Math.max(0, $dropdown.children('li.hover').index());
+        nLastIndex = $items.length-1;
+        $current = $items.eq(nHoverIndex);
         $dropdown.data('lastKeypress', +new Date());
         switch (e.which) {
           case 13: // Enter
+            if (!isOpen) toggleHover($current, 1);
             $current.click();
             return;
           case 27: // Esc
             resetDropdown($dropdown[0]);
             return;
           case 32: // Space
-            sKey = ' ';
+            if (isOpen) {
+              sKey = ' ';
+            } else {
+              toggleHover($current, 1);
+              $current.click();
+            }
             break;
           case 33: // Page Up
             toggleHover($current, 0);
-            toggleHover($dropdown.children().eq(Math.max(nHoverIndex-nItemsPerPage-1, 0)), 1);
+            toggleHover($items.eq(Math.max(nHoverIndex-nItemsPerPage-1, 0)), 1);
             return;
           case 34: // Page Down
             toggleHover($current, 0);
-            toggleHover($dropdown.children().eq(Math.min(nHoverIndex+nItemsPerPage-1, nLastIndex)), 1);
+            toggleHover($items.eq(Math.min(nHoverIndex+nItemsPerPage-1, nLastIndex)), 1);
             return;
           case 35: // End
             toggleHover($current, 0);
-            toggleHover($dropdown.children().eq(nLastIndex), 1);
+            toggleHover($items.eq(nLastIndex), 1);
             return;
           case 36: // Home
             toggleHover($current, 0);
-            toggleHover($dropdown.children().eq(0), 1);
+            toggleHover($items.eq(0), 1);
             return;
           case 38: // Up
             toggleHover($current, 0);
-            // If not already key-navigated or first item is selected, cycle to the last item;
-            // else select the previous item
-            toggleHover(nHoverIndex ? $dropdown.children().eq(nHoverIndex-1) : $dropdown.children().eq(nLastIndex), 1);
+            // If not already key-navigated or first item is selected, cycle to
+            // the last item; else select the previous item
+            toggleHover(nHoverIndex ? $items.eq(nHoverIndex-1) : $items.eq(nLastIndex), 1);
             return;
           case 40: // Down
             toggleHover($current, 0);
             // If last item is selected, cycle to the first item;
             // else select the next item
-            toggleHover(nHoverIndex===nLastIndex ? $dropdown.children().eq(0) : $dropdown.children().eq(nHoverIndex+1), 1);
+            toggleHover(nHoverIndex===nLastIndex ? $items.eq(0) : $items.eq(nHoverIndex+1), 1);
             return;
           default:
             sKey = aKeys[e.which-48];
@@ -80,11 +94,12 @@
           $dropdown.data('keysPressed', $dropdown.data('keysPressed')===undefined ? sKey : $dropdown.data('keysPressed') + sKey);
           nTimer = setTimeout(function() {
             $dropdown.removeData('keysPressed');
+            // NOTE: Windows keyboard repeat delay is 250-1000 ms. See
+            // https://technet.microsoft.com/en-us/library/cc978658.aspx
           }, 300);
           // Build index of matches
           var aMatches = [],
-            nCurrentIndex = $current.index(),
-            $items = $dropdown.children();
+            nCurrentIndex = $current.index();
           $items.each(function(nIndex) {
             if ($(this).text().toLowerCase().indexOf($dropdown.data('keysPressed'))===0) aMatches.push(nIndex);
           });
@@ -104,37 +119,48 @@
           }
         }
       },
-      resetDropdown = function(o) {
-        var $dropdown = $(o.currentTarget||o);
-        $dropdown.data('hover', false);
-        clearTimeout(nTimer);
-        nTimer = setTimeout(function() {
-          if (!$dropdown.data('hover')) {
-            if ($dropdown.hasClass('reverse')) $dropdown.prepend($dropdown.children('li:last-child'));
-            $dropdown.removeClass('active changing reverse').css('height', '');
-            $dropdown.children().removeClass('hover nohover');
-            $dropdown.removeData('clicked');
-            $(window).off('keydown', handleKeypress);
-          }
-        }, (o.type==='mouseleave' && !$dropdown.data('clicked')) ? oOptions.hoverIntent : 0);
-      },
       hoverDropdownItem = function(e) {
         var $dropdown = $(e.currentTarget);
-        if (!$dropdown.hasClass('active') || new Date()-$dropdown.data('lastKeypress')<200) return;
+        if ($dropdown[0]===e.target || !$dropdown.hasClass('active') || new Date()-$dropdown.data('lastKeypress')<200) return;
         toggleHover($dropdown.children(), 0, 1);
         toggleHover($(e.target), 1, 1);
       },
-      toggleHover = function($el, bOn, bNoScroll) {
+      resetDropdown = function(o) {
+        var $dropdown = $(o.currentTarget||o);
+        // NOTE: Sometimes it's possible for $dropdown to point to the wrong
+        // element when you quickly hover over another menu. To prevent this, we
+        // need to check for .active as a backup.
+        if (!$dropdown.hasClass('active')) $dropdown = $('.prettydropdown > ul.active');
+        $dropdown.data('hover', false);
+        clearTimeout(nTimer);
+        nTimer = setTimeout(function() {
+          if ($dropdown.data('hover')) return;
+          if ($dropdown.hasClass('reverse')) $dropdown.prepend($dropdown.children('li:last-child'));
+          $dropdown.removeClass('active reverse').css('height', '');
+          $dropdown.children().removeClass('hover nohover');
+          $dropdown.removeData('clicked').attr('aria-expanded', 'false');
+        }, (o.type==='mouseleave' && !$dropdown.data('clicked')) ? oOptions.hoverIntent : 0);
+      },
+      selectDropdownItem = function($li) {
+        var $dropdown = $li.parent(),
+          $select = $dropdown.parent().find('select');
+        $dropdown.children('li.selected').removeClass('selected');
+        $dropdown.prepend($li.addClass('selected')).removeClass('reverse').attr('aria-activedescendant', $li.attr('id'));
+        // Sync <select> element
+        $select.children('option[value="' + $li.data('value') +'"]').prop('selected', true);
+        $select.trigger('change');
+      },
+      toggleHover = function($li, bOn, bNoScroll) {
         if (bOn) {
-          $el.removeClass('nohover').addClass('hover');
-          if ($el.length===1 && $current && !bNoScroll) {
+          $li.removeClass('nohover').addClass('hover');
+          if ($li.length===1 && $current && !bNoScroll) {
             // Ensure items are always in view
-            var $dropdown = $el.parent(),
+            var $dropdown = $li.parent(),
               nDropdownHeight = $dropdown.outerHeight(),
-              nItemOffset = $el.offset().top-$dropdown.offset().top-1; // -1px for top border
-            if ($el.index()===0) {
+              nItemOffset = $li.offset().top-$dropdown.offset().top-1; // -1px for top border
+            if ($li.index()===0) {
               $dropdown.scrollTop(0);
-            } else if ($el.index()===nLastIndex) {
+            } else if ($li.index()===nLastIndex) {
               $dropdown.scrollTop($dropdown.children().length*oOptions.height);
             } else {
               if (nItemOffset+oOptions.height>nDropdownHeight) $dropdown.scrollTop($dropdown.scrollTop()+oOptions.height+nItemOffset-nDropdownHeight);
@@ -142,41 +168,50 @@
             }
           }
         } else {
-          $el.removeClass('hover').addClass('nohover');
+          $li.removeClass('hover').addClass('nohover');
         }
       };
     // Validate options
     if (isNaN(oOptions.height) || oOptions.height<8) oOptions.height = 8;
     if (isNaN(oOptions.hoverIntent) || oOptions.hoverIntent<0) oOptions.hoverIntent = 200;
     return this.each(function() {
-      var $this = $(this);
-      if ($this.data('loaded')) return true; // Continue
-      $this.outerHeight(oOptions.height);
-      // NOTE: $this.css('margin') returns empty string in Firefox.
-      // See https://github.com/jquery/jquery/issues/3383
-      var nWidth = $this.outerWidth(),
+      var $select = $(this);
+      if ($select.data('loaded')) return true; // Continue
+      $select.outerHeight(oOptions.height);
+      var nCount = 0,
+        nTimestamp = +new Date(),
+        nWidth = $select.outerWidth(),
         // Height - 2px for borders
-        sHtml = '<ul' + ($this.attr('title')?' title="'+$this.attr('title')+'"':'') + ' style="max-height:' + (oOptions.height-2) + 'px;margin:'
-          + $this.css('margin-top') + ' '
-          + $this.css('margin-right') + ' '
-          + $this.css('margin-bottom') + ' '
-          + $this.css('margin-left') + ';">',
+        sHtml = '<ul' + ($select.attr('title')?' title="'+$select.attr('title')+'"':'')
+          + ' tabindex="0" role="listbox" aria-activedescendant="item' + nTimestamp
+          + '-1" aria-expanded="false" style="max-height:' + (oOptions.height-2) + 'px;margin:'
+          // NOTE: $select.css('margin') returns empty string in Firefox.
+          // See https://github.com/jquery/jquery/issues/3383
+          + $select.css('margin-top') + ' '
+          + $select.css('margin-right') + ' '
+          + $select.css('margin-bottom') + ' '
+          + $select.css('margin-left') + ';">',
         renderItem = function(el, sClass) {
-          return '<li data-value="' + el.value + '"'
+          return '<li id="item' + nTimestamp + '-' + nCount
+            + '" data-value="' + el.value + '"'
             + (el.title ? ' title="' + el.title + '"' : '')
             + (sClass ? ' class="' + sClass + '"' : '')
-            + ((oOptions.height!==50) ? ' style="height:' + (oOptions.height-2) + 'px;line-height:' + (oOptions.height-2) + 'px"' : '')
+            + ' role="option"'
+            + ((oOptions.height!==50) ? ' style="height:' + (oOptions.height-2)
+            + 'px;line-height:' + (oOptions.height-2) + 'px"' : '')
             + '>' + el.text + '</li>';
         };
-      $this.children('option:selected').each(function() {
+      $select.children('option:selected').each(function() {
+        ++nCount;
         sHtml += renderItem(this, 'selected');
       });
-      $this.children('option:not(:selected)').each(function() {
+      $select.children('option:not(:selected)').each(function() {
+        ++nCount;
         sHtml += renderItem(this);
       });
       sHtml += '</ul>';
-      $this.css('visibility', 'hidden').wrap('<div class="prettydropdown ' + oOptions.customClass + ' loading"></div>').before(sHtml).data('loaded', true);
-      var $dropdown = $this.parent().children('ul'),
+      $select.css('visibility', 'hidden').wrap('<div class="prettydropdown ' + oOptions.customClass + ' loading"></div>').before(sHtml).data('loaded', true);
+      var $dropdown = $select.parent().children('ul'),
         nWidth = $dropdown.outerWidth(true),
         nOuterWidth;
       // Calculate width if initially hidden
@@ -195,20 +230,18 @@
       // NOTE: Setting width using width(), then css() because width() only can
       // return a float, which can result in a missing right border when there
       // is a scrollbar.
-      $dropdown.children('li').width(nWidth);
-      $dropdown.children('li').css('width', $dropdown.children('li').css('width')).click(function() {
-        var $li = $(this);
+      $dropdown.children().width(nWidth).css('width', $dropdown.children().css('width')).click(function() {
         // Only update if different value selected
-        if ($dropdown.hasClass('active') && $li.data('value')!==$dropdown.children('li.selected').data('value')) {
-          $dropdown.children('li.selected').removeClass('selected');
-          $dropdown.prepend($li.addClass('selected')).removeClass('reverse');
-          // Sync <select> element
-          $this.children('option[value="' + $li.data('value') +'"]').prop('selected', true);
-          $this.trigger('change');
+        if ($dropdown.hasClass('active') && $(this).data('value')!==$dropdown.children('li.selected').data('value')) {
+          selectDropdownItem($(this));
         }
         $dropdown.toggleClass('active');
         // Try to keep drop-down menu within viewport
         if ($dropdown.hasClass('active')) {
+          // Close any other open menus
+          if ($('.prettydropdown > ul.active').length>1) {
+            resetDropdown($('.prettydropdown > ul.active').not($dropdown)[0]);
+          }
           var nWinHeight = window.innerHeight,
             nOffsetTop = $dropdown.offset().top,
             nScrollTop = document.body.scrollTop,
@@ -226,13 +259,20 @@
               $dropdown.height($dropdown.height()-(nDropdownBottom-nWinHeight));
             }
           }
-          $(window).on('keydown', handleKeypress);
+          $dropdown.attr('aria-expanded', 'true');
         } else {
-          $dropdown.addClass('changing').data('clicked', true); // Prevent FOUC
+          $dropdown.data('clicked', true);
           resetDropdown($dropdown[0]);
         }
       });
       $dropdown.on({
+        focusin: function() {
+          // Unregister any existing handlers first to prevent duplicate firings
+          $(window).off('keydown', handleKeypress).on('keydown', handleKeypress);
+        },
+        focusout: function() {
+          $(window).off('keydown', handleKeypress);
+        },
         mouseenter: function() {
           $dropdown.data('hover', true);
         },
